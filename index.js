9e32c735c9051fabac3da26b005452cd693a9001
michael-mbugua <michael.mbugua@student.moringaschool.com>
2022-08-24 15:23:29 +0300

    arrow function resubmitted

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-const sum=( (parameter1,parameter2)=>{
-    let add=parameter1 + parameter2;
-    console.log(add)
-})(2,3)
+function divide(){
+    let divides=2000/100;
+    console.log(divides)
+    return divides
+}
+divide()
+
+square=(num1)=>{
+    let math2=num1 * num1;
+    console.log(math2)
+    return math2
+}
+square(2);
+let add=(num2,num3)=>{
+    let math3=num2+num3;
+    console.log(math3)
+    return math3
+}
+add(2,5)
